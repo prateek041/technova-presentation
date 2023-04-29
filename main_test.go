8bbd425855436ac9468cba42a19fece919e10118
prateek041 <prateeksingh9741@gmail.com>
2023-04-29 09:12:46 +0530

    Added changev2
    Signed-off-by: prateek041 <prateeksingh9741@gmail.com>

--- main_test.go
+++ main_test.go
@@ -22,7 +22,7 @@ func TestHandler(t *testing.T) {
 			status, http.StatusOK)
 	}
 
-	expected := "Hello this is the first route"
+	expected := "TThis is the Hello"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
